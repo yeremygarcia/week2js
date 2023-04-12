211ea339d87916fc2908475adc05ab6a609ab985
yeremygarcia <jeremyisgarcia@gmail.com>
2023-04-11 22:20:10 -0400

    2

--- class.js
+++ class.js
@@ -17,9 +17,59 @@ class job {
     return this.name;
   }
   getFieldAndIndustry() {
-    return this.field;
+    return `${this.field} and ${this.industry}`;
   }
   getSalary() {
     return this.avgSalary;
   }
 }
+
+const job1 = new Job("Accountant", "Finance", true, "Accounting", "$99,999");
+
+console.log(job1.getName());
+console.log(job1.getFieldAndIndustry());
+
+const job2 = new Job("Teacher", "Education", true, "CS", "$69,999");
+
+console.log(job2.getName());
+console.log(job2.getSalary());
+
+const job3 = new Job("MD", "Healthcare", true, "HCP", "$199,999");
+
+console.log(job3.getName());
+console.log(job3.getInformation());
+
+class Career extends Job {
+  constructor(
+    name,
+    industry,
+    requiresDegree,
+    field,
+    avgSalary,
+    yearsOfExperience,
+    jobDescription
+  ) {
+    super(name, industry, requiresDegree, field, avgSalary);
+    this.yearsOfExperience = yearsOfExperience;
+    this.jobDescription = jobDescription;
+  }
+
+  getYearsOfExperience() {
+    return this.yearsOfExperience;
+  }
+
+  getJobDescription() {
+    return this.jobDescription;
+  }
+}
+let career1 = new Career(
+  "Software Engineer",
+  "Computer Science",
+  true,
+  "Technology",
+  "$299,999",
+  ".5",
+  "coder"
+);
+console.log(career1.getYearsOfExperience());
+console.log(career1.getJobDescription());
